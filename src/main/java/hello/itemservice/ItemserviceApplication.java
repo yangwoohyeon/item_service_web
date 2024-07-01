133d8f357a67e67e6df6d0a8f953f1e2fb9c8f86
yangwoohyeon <dngus0717@gmail.com>
2024-07-01 15:35:29 +0900

    web project

--- src/main/java/hello/itemservice/ItemserviceApplication.java
+++ src/main/java/hello/itemservice/ItemserviceApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ItemserviceApplication { //spring 강의 프로젝트
+public class ItemserviceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ItemserviceApplication.class, args);
