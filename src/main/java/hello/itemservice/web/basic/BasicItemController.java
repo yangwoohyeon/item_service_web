b0788e55f25d96446bf5368d9625524dbfa34075
yangwoohyeon <dngus0717@gmail.com>
2024-07-05 15:06:52 +0900

    web project

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -23,11 +24,19 @@ public class BasicItemController {
 //    public BasicItemController(ItemRepository itemRepository) {
 //        this.itemRepository = itemRepository;
 //    }
+
     @GetMapping
     public String items(Model model){
         List<Item> items = itemRepository.findAll();
         model.addAttribute("items",items);
         return "basic/items";
+    }
+
+    @GetMapping("/{itemId}")
+    public String item(@PathVariable Long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "basic/item";
     }
     /*
     * 테스트용 데이터 추가
