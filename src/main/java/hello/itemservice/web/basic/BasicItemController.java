9ba2d2520430646993d30ba12be7c9c4c429d68f
yangwoohyeon <dngus0717@gmail.com>
2024-07-05 16:06:04 +0900

    web project

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -38,6 +38,12 @@ public class BasicItemController {
         model.addAttribute("item", item);
         return "basic/item";
     }
+
+    @GetMapping("/add")
+    public String addForm(){
+        return "basic/addForm";
+    }
+
     /*
     * 테스트용 데이터 추가
     */
