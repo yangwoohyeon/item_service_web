1481a826b7dfd3bc5c14951f8df97d2591427499
yangwoohyeon <dngus0717@gmail.com>
2024-07-05 16:16:58 +0900

    web project

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -41,6 +42,11 @@ public class BasicItemController {
 
     @GetMapping("/add")
     public String addForm(){
+        return "basic/addForm";
+    }
+
+    @PostMapping("/add")   //HTTP 메서드로 기능 구분
+    public String save(){
         return "basic/addForm";
     }
 
